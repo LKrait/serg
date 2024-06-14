6dff4352c62bf486da004320ec81d5cd8e37c00a
Kevin Afio <admin@example.com>
2024-06-14 20:58:24 +1000

    Adding loging/logout views

--- serg/serg/urls.py
+++ serg/serg/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from user import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sitemap.urls')),
-    path('asset/', include('asset.urls'))
+    path('asset/', include('asset.urls'), name='asset'),
+    path('st/login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('st/logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout')
 ]
+
--- serg/user/models.py
+++ serg/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Account(models.Model):
+    account_id = models.CharField(max_length=20)
+    account_name = models.CharField(max_length=50)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"[ Customer-{self.account_id} ] : {self.account_name}"
