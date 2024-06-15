3fadb19d151bd532b8d13ab85a90e50b74271d1d
Kevin Afio <admin@example.com>
2024-06-16 03:07:06 +1000

    Enabling email functionality on dashboard loading notification

--- serg/asset/views.py
+++ serg/asset/views.py
@@ -10,7 +10,7 @@ from .models import Asset
 @login_required
 def dashboard(request):
     info = "Access Notice: "+str(random.randrange(1000,9999,1))
-    # send_mail(info, f"Dashboard_001 has been accessed from IP=127.0.0.1:8000 on {time.ctime()}", "afiokevin@gmail.com", ["afio.kev@gmail.com"], fail_silently=True)
+    send_mail(info, f"Dashboard_001 has been accessed from IP=127.0.0.1:8000 on {time.ctime()}", "afiokevin@gmail.com", ["afio.kev@gmail.com"], fail_silently=True)
     # asset = Asset.objects.all()
     return render(request, 'asset/dashboard.html', {'asset': 'asset'})
 
